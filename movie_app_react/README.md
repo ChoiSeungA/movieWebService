0d6d1d7be0d64360e8097b1eba1d8c873dc66201
ChoiSeungA <21800731@handong.edu>
2021-03-30 14:29:20 +0900

    First day push!

---
+++ movie_app_react/README.md
@@ -0,0 +1,4 @@
+# Movie App
+
+React JS Fundamentals Course!
+리액트 기반으로 영화 서비스 웹 만들기!
---
+++ movie_app_react/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Potato from './Potato';
+
+function App() {
+  return (
+    <div className="App">
+      hello~
+      <Potato />
+    </div>
+  );
+}
+
+export default App;
