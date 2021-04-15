e0c2465a9ebad58e40b2d4a614346bb6c7e2a90d
ChoiSeungA <21800731@handong.edu>
2021-04-15 12:47:42 +0900

    css finish

--- movie_app_react/src/App.js
+++ movie_app_react/src/App.js
@@ -1,80 +1,50 @@
 //https://nomadcoders.co/react-fundamentals/lobby
-// #3 STATE 할 차례!
-
 import React from 'react';
+import axios from "axios";
 import PropTypes from "prop-types";
+import Movie from './Movie';
+import "./App.css"
 
-function Food({ name , picture, rating}) {
-  return <div>
-    <h2>I like {name}</h2>
-    <h4>{rating} / 5.0</h4>
-    <img src={picture} alt={name}></img>
-  </div>
-}
+class App extends React.Component{
+  state = {
+    isLoading: true,
+    movies: []
+    // 미래에 사용할 변수를 굳이 처음부터 여기에 선언할 필요는 없음!
+  };
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-}
-// required, type check를 할 수 있음!!
+  getMovies = async () => {
+    // async, await 너는 이거 끝날때까지 기다려야해
+    // const movies = await axios.get("https://yts-proxy.now.sh/list_moives.json");
+    // console.log(movies.data.data.movies);
+    const {data: {data: {movies}}} = await axios.get("https://yts.mx/api/v2/list_movies.json");
+    this.setState({movies: movies, isLoading:false});
+  };
 
-const foodILike = [
-  {
-    id:1,
-    name: "Kimchi",
-    image: "https://cdn.crowdpic.net/detail-thumb/thumb_d_CDC14868821FF3F20C77BC8BC15E6355.jpg",
-    rating: 4.7
-  },
-  { 
-    id:2,
-    name: "삼겹살",
-    image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwoman.chosun.com%2Fmobile%2Fnews%2Fview.asp%3Fcate%3DC05%26mcate%3DM1001%26nNewsNumb%3D20200868344&psig=AOvVaw1PDIe_YRZfYP376cyl9Jaw&ust=1618127180459000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKjw0tCX8-8CFQAAAAAdAAAAABAD",
-    rating: 4.0
-  },
-  {
-    id:3,
-    name: "비빔밥", 
-    image: "https://lh3.googleusercontent.com/proxy/3yVmq_QLhpBBbTkZ1G65O25BVAF03Ebru9Dub6aYfPCeCcXxua8uMV6pVPv20tpXmKu8vLcvk1EsIFYAMrE98LSObZvE2BKjkmM",
-    rating: 3.2
-  },
-  {
-    id:4,
-    name: "Kimchi",
-    image: "https://cdn.crowdpic.net/detail-thumb/thumb_d_CDC14868821FF3F20C77BC8BC15E6355.jpg",
-    rating: 2.7
-  },
-  {
-    id:5,
-    name: "삼겹살",
-    image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwoman.chosun.com%2Fmobile%2Fnews%2Fview.asp%3Fcate%3DC05%26mcate%3DM1001%26nNewsNumb%3D20200868344&psig=AOvVaw1PDIe_YRZfYP376cyl9Jaw&ust=1618127180459000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKjw0tCX8-8CFQAAAAAdAAAAABAD",
-    rating: 4.8
-  },
-  {
-    id:6,
-    name: "비빔밥",
-    image: "https://lh3.googleusercontent.com/proxy/3yVmq_QLhpBBbTkZ1G65O25BVAF03Ebru9Dub6aYfPCeCcXxua8uMV6pVPv20tpXmKu8vLcvk1EsIFYAMrE98LSObZvE2BKjkmM",
-    rating: 3.1
+  async componentDidMount(){
+    // axios는 시간이 좀 걸림, 끝나는거 기다려야해!! = async or function
+    this.getMovies();
+  };
+
+  render(){
+    const { isLoading, movies } = this.state;
+    return (
+    <section className="container">
+      {isLoading ? 
+      (
+        <div className="loader">
+          <span className="loader_text">Loading...</span>
+        </div>
+      ): 
+      (
+        <div className="movies">
+          {movies.map(movie => {
+            return <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image } genres={movie.genres} />
+          })}
+        </div>
+      )
+      }</section>
+    )
   }
-];
-
-function renderFood(dish) {
-  return <Food name={dish.name} picture={dish.image}/>
-}
-
-function App() {
-  return (
-    <div className="App">
-      {/* {console.log(foodILike.map(renderFood))}
-      {foodILike.map(renderFood)} */}
-      {foodILike.map(dish => 
-      <Food 
-        key={dish.id} 
-        name={dish.name} 
-        picture={dish.image}
-        rating={dish.rating}/>)}
-    </div>
-  );
 }
 
 export default App;
